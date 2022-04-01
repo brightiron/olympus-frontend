6c4016563b053d8718bc204722f827f1e5642d3b
brightiron <brightiron@protonmail.com>
2022-03-31 22:14:39 -0500

    updating tests

--- src/views/Stake/__tests__/Stake.unit.test.tsx
+++ src/views/Stake/__tests__/Stake.unit.test.tsx
@@ -1,7 +1,6 @@
 import { fireEvent } from "@testing-library/dom";
-import { MockProvider } from "ethereum-waffle";
 import * as useWeb3Context from "src/hooks/web3Context";
-import { ReactQueryProvider } from "src/lib/react-query";
+import { mockWeb3Context } from "src/testHelpers";
 
 import { render, screen } from "../../../testUtils";
 import Stake from "../Stake";
@@ -10,55 +9,28 @@ afterEach(() => {
   jest.restoreAllMocks();
 });
 describe("<Stake/>", () => {
-  const provider = new MockProvider();
   it("should render component", async () => {
-    const { container } = await render(
-      <ReactQueryProvider>
-        <Stake />
-      </ReactQueryProvider>,
-    );
+    const { container } = await render(<Stake />);
     expect(container).toMatchSnapshot();
   });
 
   it("should render the stake Input Area when connected", async () => {
     const data = jest.spyOn(useWeb3Context, "useWeb3Context");
-    const testing = data.mockReturnValue({
-      ...data.mock.results,
-      provider,
-      networkId: 1,
-      connected: true,
-    });
-    const { container } = await render(
-      <ReactQueryProvider>
-        <Stake />
-      </ReactQueryProvider>,
-    );
+    data.mockReturnValue(mockWeb3Context);
+    const { container } = await render(<Stake />);
     expect(container).toMatchSnapshot();
   });
 
   it("should render gOHM conversion when toggling to gOHM", async () => {
     const data = jest.spyOn(useWeb3Context, "useWeb3Context");
-    const testing = data.mockReturnValue({
-      ...data.mock.results,
-      provider,
-      networkId: 1,
-      connected: true,
-    });
-    const { container } = await render(
-      <ReactQueryProvider>
-        <Stake />
-      </ReactQueryProvider>,
-    );
+    data.mockReturnValue(mockWeb3Context);
+    const { container } = await render(<Stake />);
     fireEvent.click(screen.getByLabelText("stake to gohm"));
     expect(container).toMatchSnapshot();
   });
 
   it("should render correct staking headers", async () => {
-    const { container } = await render(
-      <ReactQueryProvider>
-        <Stake />
-      </ReactQueryProvider>,
-    );
+    const { container } = await render(<Stake />);
     // there should be a header inviting user to Stake
     expect(await screen.getByText("Single Stake (3, 3)")).toBeInTheDocument();
     //  there should be a Farm Pool table
@@ -67,11 +39,7 @@ describe("<Stake/>", () => {
   });
 
   it("should render all supported multi chain staking contracts", async () => {
-    const { container } = await render(
-      <ReactQueryProvider>
-        <Stake />
-      </ReactQueryProvider>,
-    );
+    const { container } = await render(<Stake />);
     expect(await screen.getByText("gOHM-AVAX")).toBeInTheDocument();
     expect(await screen.getByText("Stake on Trader Joe").closest("a")).toHaveAttribute(
       "href",
